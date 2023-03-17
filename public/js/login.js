ec5c8d66fb9736259edf76be53ac832beb1c8e0a
Jessica Zelasky <jzelasky@gmail.com>
2023-03-16 20:08:36 -0400

    login page works

--- public/js/login.js
+++ public/js/login.js
@@ -1,4 +1,7 @@
+
+
 const loginFormHandler = async (event) => {
+  console.log("test")
   // Stop the browser from submitting the form so we can do so with JavaScript
   event.preventDefault();
 
@@ -23,5 +26,33 @@ const loginFormHandler = async (event) => {
 };
 
 document
-  .querySelector('.login-form')
-  .addEventListener('submit', loginFormHandler);
+  .getElementById('login-btn')
+  .addEventListener('click', loginFormHandler);
+
+/*
+const createUserHandler = async (event) => {
+  // Stop the browser from submitting the form so we can do so with JavaScript
+  event.preventDefault();
+
+  // Gather the data from the form elements on the page
+  const name = document.querySelector('#name-create').value.trim();
+  const email = document.querySelector('#email-create').value.trim();
+  const password = document.querySelector('#password-create').value.trim();
+
+  if (name && email && password) {
+    // Send the e-mail and password to the server
+    const response = await fetch('/api/users/login', {
+      method: 'POST',
+      body: JSON.stringify({ name, email, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      document.location.replace('/');
+    } else {
+      alert('Failed to log in');
+    }
+  }
+};
+*/
+
